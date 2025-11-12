0029c22e7c7fe040d38c87b23d7b7c0b5095e23c
siddhant <siddhskore@gmail.com>
2025-11-12 21:50:28 +0530

    try: add description

--- src/main/java/com/nival/chit/enums/ChitGroupStatus.java
+++ src/main/java/com/nival/chit/enums/ChitGroupStatus.java
@@ -1,8 +1,15 @@
 package com.nival.chit.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum ChitGroupStatus {
-    ACTIVE,
-    COMPLETED,
-    PENDING,
-    INACTIVE // try to give duration
+    ACTIVE("Group is active"),
+    COMPLETED("Group Completed and closed"),
+    PENDING("Group Operation is pending"),
+    INACTIVE("Group is inactive"); // try to give duration
+
+    private final String description;
 }
