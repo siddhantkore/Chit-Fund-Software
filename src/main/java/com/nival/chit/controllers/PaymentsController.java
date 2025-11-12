67d97e7f035487f7e50ae09aee2814d7a5720543
siddhant <siddhskore@gmail.com>
2025-11-12 21:55:02 +0530

    configure required controllers *

---
+++ src/main/java/com/nival/chit/controllers/PaymentsController.java
@@ -0,0 +1,9 @@
+package com.nival.chit.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chit/payments/")
+public class PaymentsController {
+}
---
+++ src/main/java/com/nival/chit/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.nival.chit.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/chit/user/")
+@RequiredArgsConstructor
+public class UserController {
+
+}
