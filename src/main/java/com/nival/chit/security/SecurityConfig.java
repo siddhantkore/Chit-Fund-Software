5259a021f9e8af9d905360ba3e5a3a44ad394ac5
siddhant <siddhskore@gmail.com>
2025-12-01 15:48:06 +0530

    Implement the spring security configuration

--- src/main/java/com/nival/chit/security/SecurityConfig.java
+++ src/main/java/com/nival/chit/security/SecurityConfig.java
@@ -3,21 +3,20 @@ package com.nival.chit.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration {
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()  // public APIs
+                        .requestMatchers("/api/auth/user").permitAll()  // public APIs
                         .anyRequest().authenticated()                // everything else needs login
                 )
                 .httpBasic(Customizer.withDefaults())               // basic auth
